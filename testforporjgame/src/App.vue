c6334f7884d8c0614e93c46f81e68a71c44a3aae
hunnymc <thanaphon.mc@mail.kmutt.ac.th>
2022-02-19 18:12:31 +0700

    Add new project

---
+++ testforporjgame/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const textKey = ref([])
+document.body.addEventListener('keydown', (e) => {
+  textKey.value.push(e.which)
+});
+
+</script>
+ 
+<template>
+  <div style="margin: 2em;">
+    <h1>{{ textKey }}</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
