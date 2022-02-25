8ac41fd30339a1657b5a292b9bb3975927d280df
hunnymc <thanaphon.mc@mail.kmutt.ac.th>
2022-02-26 00:19:19 +0700

    update

---
+++ testforporjgame/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [require("daisyui")],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
