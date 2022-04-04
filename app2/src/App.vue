f8b950a0a6bd42825a1cb4eb8fe3fe633fda59e2
hunnymc <thanaphon.mc@mail.kmutt.ac.th>
2022-04-05 02:36:53 +0700

    up

--- app2/src/App.vue
+++ app2/src/App.vue
@@ -1,21 +1,27 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import BaseButtonVue from './components/BaseButton.vue';
+const testMe1 = () => console.log('test me 1')
+const testMe2 = (e) => console.log('test me 2', e)
+const testMe3 = (e) => console.log('test me 3', e)
+const testMe4 = (e, n) => console.log('test me 4', e, n)
+const testMe5 = (e, n) => console.log('test me 5', e, n)
+const testMe6 = (e, n) => console.log('test me 6', e, n)
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div>
+    <div>
+      <button @click="testMe1">TEST ME 1</button>
+      <button @click="testMe2">TEST ME 2</button>
+
+      <button @click="testMe3()">TEST ME 3</button>
+      <button @click="testMe4($event)">TEST ME 4</button>
+
+      <button @click="testMe5($event, 999)">TEST ME 5</button>
+      <button @click="testMe6(999, $event)">TEST ME 6</button>
+    </div>
+    <BaseButtonVue></BaseButtonVue>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ app2/src/components/BaseButton.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <button>OK</button>
+</template>
+
+<style></style>
